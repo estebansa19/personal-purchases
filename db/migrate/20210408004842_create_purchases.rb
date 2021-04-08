b85994083876e3b68e242795632b423370093b6b
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2021-04-07 19:51:24 -0500

    create Purchase model and set user at database.yml file

---
+++ db/migrate/20210408004842_create_purchases.rb
@@ -0,0 +1,10 @@
+class CreatePurchases < ActiveRecord::Migration[6.1]
+  def change
+    create_table :purchases do |t|
+      t.string :title
+      t.string :value, null: true, default: 0
+
+      t.timestamps
+    end
+  end
+end
