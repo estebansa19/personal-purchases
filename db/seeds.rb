ab709a306b588452099e5e44c33e8791d5e668e5
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2022-09-13 18:52:17 -0500

    create base seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+records_attributes = [
+  {
+    title: 'food', value: 66666,
+  },
+  {
+    title: 'cellphone', value: 1000000,
+  },
+  {
+    title: 'clothes', value: 100000,
+  },
+  {
+    title: 'coffee', value: 10000,
+  },
+  {
+    title: 'transport', value: 100000,
+  }
+]
+
+Purchase.insert_all(records_attributes)
